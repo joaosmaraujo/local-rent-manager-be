3632b33bd60a4b7eb964453cf249eaada3346f30
Joao Miguel Araujo <araujoj2@MACC02X43VWJGH5.local>
2020-01-08 11:02:12 +0000

    populate customers model with corresponding houses

--- app/models/customers.js
+++ app/models/customers.js
@@ -1,5 +1,6 @@
 const mongoose = require("../database/mongoose.js");
-;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
 const CustomerSchema = new mongoose.Schema({
     firstName: {
         type: String,
@@ -9,6 +10,7 @@ const CustomerSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    houses: [{ type: ObjectId, ref: 'House' }],
     createAt: {
         type: Date,
         default: Date.now()
