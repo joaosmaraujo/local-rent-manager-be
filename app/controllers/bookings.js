c387718a4b22069d1179075c6119a92935dbebe0
Joao Miguel Araujo <araujoj2@MACC02X43VWJGH5.local>
2020-01-19 14:24:48 +0000

    update arrays of related documents when there are changes

--- app/controllers/bookings.js
+++ app/controllers/bookings.js
@@ -1,3 +1,4 @@
+const House = require("../models/houses");
 const AppController = require('./app');
 /**
  * The App controller class where other controller inherits or
@@ -11,6 +12,58 @@ class BookingController extends AppController {
 	 */
 	constructor(model) {
 		super(model);
+	}
+
+		/**
+	 * @param {Object} req The request object
+	 * @param {Object} res The response object
+	 * @return {Object} res The response object
+	 */
+	async add(req, res) {
+        try {
+			const boooking = await this._model.create(req.body);
+			const house = await House.findOne({ _id: boooking.house });
+			house.bookings.push(boooking._id);
+			await House.findByIdAndUpdate(house._id, house);
+            return res.send({
+                name: "added object",
+                content: { task },
+                status: 200,
+                success: true
+            });
+        } catch (err) {
+            return res.status(400).send({ error: "Could not add house. " + err });
+        }
+	}
+
+	/**
+	 * @param {Object} req The request object
+	 * @param {Object} res The response object
+	 * @return {Object} res The response object
+	 */
+	async update (req, res) {
+		const _id = req.params.id;
+		const error = "Could not edit object.";
+		try {
+			const boooking = await this._model.findOne({ _id })
+			if (boooking) {
+				if (boooking.house !== req.body.house._id) {
+					const previousHouse = await House.findOne({ _id: boooking.house });
+					const newHouse = await House.findOne({ _id: req.body.house._id });
+					previousHouse.boookings.splice(previousHouse.boookings.findIndex(item => item._id === boooking._id), 1)
+					newHouse.boookings.push(req.body._id);
+					await House.findByIdAndUpdate(previousHouse._id, previousHouse);
+					await House.findByIdAndUpdate(newHouse._id, newHouse);
+				}
+				await this._model.findByIdAndUpdate(_id, req.body);
+				return res.send();
+			} else {
+				return res.status(404).send({ error: error + `Cannot find id '${_id}'`});
+			}
+		} catch (err) {
+			return res.status(400).send({ error: error + err });
+		}
+		
 	}
 
 	async getAll(req, res) {
--- app/controllers/houses.js
+++ app/controllers/houses.js
@@ -1,3 +1,4 @@
+const Customer = require("../models/customers");
 const AppController = require('./app');
 /**
  * The App controller class where other controller inherits or
@@ -11,6 +12,58 @@ class HouseController extends AppController {
 	 */
 	constructor(model) {
 		super(model);
+	}
+
+	/**
+	 * @param {Object} req The request object
+	 * @param {Object} res The response object
+	 * @return {Object} res The response object
+	 */
+	async add(req, res) {
+        try {
+			const house = await this._model.create(req.body);
+			const customer = await Customer.findOne({ _id: house.customer });
+			customer.houses.push(house._id);
+			await Customer.findByIdAndUpdate(customer._id, customer);
+            return res.send({
+                name: "added object",
+                content: { task },
+                status: 200,
+                success: true
+            });
+        } catch (err) {
+            return res.status(400).send({ error: "Could not add house. " + err });
+        }
+	}
+
+	/**
+	 * @param {Object} req The request object
+	 * @param {Object} res The response object
+	 * @return {Object} res The response object
+	 */
+	async update (req, res) {
+		const _id = req.params.id;
+		const error = "Could not edit object.";
+		try {
+			const house = await this._model.findOne({ _id })
+			if (house) {
+				if (house.customer !== req.body.customer._id) {
+					const previousCustomer = await Customer.findOne({ _id: house.customer });
+					const newCustomer = await Customer.findOne({ _id: req.body.customer._id });
+					previousCustomer.houses.splice(previousCustomer.houses.findIndex(item => item._id === house._id), 1)
+					newCustomer.houses.push(req.body._id);
+					await House.findByIdAndUpdate(previousCustomer._id, previousCustomer);
+					await House.findByIdAndUpdate(newCustomer._id, newCustomer);
+				}
+				await this._model.findByIdAndUpdate(_id, req.body);
+				return res.send();
+			} else {
+				return res.status(404).send({ error: error + `Cannot find id '${_id}'`});
+			}
+		} catch (err) {
+			return res.status(400).send({ error: error + err });
+		}
+		
 	}
 
 	async getAll(req, res) {
@@ -43,7 +96,9 @@ class HouseController extends AppController {
 			this._model.findOne({ _id })
 						.populate('owner')
 						.populate('bookings')
-						.populate('tasks')
+						.populate({
+							path: 'tasks',
+							populate: { path: 'work '} })
 						.exec(function(err, house) {
 							if (err) {
 								return res.status(404).send({ error: error + `Cannot find id '${_id}'`});
